5358d8450587978f7501f8d7b03bbd9ef81494d1
Shadowind201 <enzocarnier@gmail.com>
2024-05-09 20:05:11 -0300

    Adicionar arquivos de projeto.

---
+++ C# 001/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace C__001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(159, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clique no Botão";
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn1.Location = new System.Drawing.Point(202, 181);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(114, 39);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Botão =)";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label2.Location = new System.Drawing.Point(452, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Clique no outro Botão";
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btn2.Location = new System.Drawing.Point(518, 181);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(114, 39);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "Botão 2 o_0";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ C# 001/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C__001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Meu primeiro programa C#");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Oi");
+            MessageBox.Show("Tudo Bem?");
+        }
+    }
+}
